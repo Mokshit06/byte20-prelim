1134e3a7379ea70a0d0ce4a7da8c0cbce9461934
Ansh Sahni <sahniansh2004@gmail.com>
2020-08-23 21:53:25 +0530

    mapRefresh

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,12 +16,18 @@ router.get('/abcd', function(req,res,next) {
 })
 router.post('/api', function(req,res,next){
   console.log(req.body)
-  res.json({arr: random().split(',')})
+  res.json({randomCoordinate: randomCoordinate()})
 })
-
-console.log()
-
+// 
 function randomCoordinate(){
-
+  let x = Math.random()
+  if( x > 0.5){
+    return {lat: -(Math.ceil(Math.random() * 90)), long: Math.ceil(Math.random()* 180)}
+  }
+  if(x > 0.25){
+    return {lat: Math.ceil(Math.random() * 90), long: Math.ceil(Math.random()* 180)}
+  }
+  return {lat: Math.ceil(Math.random() * 90), long: -(Math.ceil(Math.random()* 180))}
 }
+// console.log(randomCoordinate)
 module.exports = router;
